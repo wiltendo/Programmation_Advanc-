468618b15e9b9705a00890a0602163862e320565
wiltendo <129499132+wiltendo@users.noreply.github.com>
2024-12-13 09:03:24 +0100

    Ajout Dossier de Calcul de la Scalabilité

---
+++ Mesure/Assignment102Fort.java
@@ -0,0 +1,54 @@
+// Estimate the value of Pi using Monte-Carlo Method, using parallel program
+package Mesure;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+class PiMonteCarloFort {
+	AtomicInteger nAtomSuccess;
+	int nThrows;
+	double value;
+	class MonteCarlo implements Runnable {
+		@Override
+		public void run() {
+			double x = Math.random();
+			double y = Math.random();
+			if (x * x + y * y <= 1)
+				nAtomSuccess.incrementAndGet();
+		}
+	}
+	public PiMonteCarloFort(int i) {
+		this.nAtomSuccess = new AtomicInteger(0);
+		this.nThrows = i;
+		this.value = 0;
+	}
+	public double getPi() throws IOException {
+		int nProcessors = Runtime.getRuntime().availableProcessors();
+		PrintWriter writer = new PrintWriter(new FileWriter("C:/Users/willi/IdeaProjects/All/Pro_Dev/src/Mesure/output.txt"));
+		for (int e = 1; e <= nProcessors; e++) {
+			nAtomSuccess = new AtomicInteger(0);
+			long startTime = System.currentTimeMillis();
+			ExecutorService executor = Executors.newWorkStealingPool(nProcessors); //nombre de Processeur detecter
+			for (int i = 1; i <= nThrows; i++) {
+				Runnable worker = new MonteCarlo();
+				executor.execute(worker); // start runnable
+			}
+			executor.shutdown(); // arreter l'execution paraléle
+			while (!executor.isTerminated()) { // tant que executor n'on terminer
+			}
+			value = 4.0 * nAtomSuccess.get() / nThrows; //Montre la valeur
+			long stopTime = System.currentTimeMillis();
+			writer.println(value + ";" + nThrows + ";" + e + ";" + (stopTime - startTime));
+		}
+		writer.close();
+		return value;  //valeur retourner
+	}
+}
+public class Assignment102Fort {
+	public static void main(String[] args) throws IOException {
+		PiMonteCarloFaible PiVal = new PiMonteCarloFaible(10000000);
+		PiVal.getPi();
+	}
+}
---
+++ Mesure/AutoPiForte.java
@@ -0,0 +1,102 @@
+package Mesure;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class AutoPiForte {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Scanner saisie = new Scanner(System.in);
+        System.out.println("Entre le nombre de Points :");
+        int nT = saisie.nextInt();
+
+        System.out.println("Entre le nombre de Processeur :");
+        int nP = saisie.nextInt();
+
+        System.out.println("Entre le nombre de Boucle :");
+        int nB = saisie.nextInt();
+
+        new Master().doRun(nT, nP,nB);
+    }
+
+}
+
+class Worker implements Callable<Long>
+{
+    private int numIterations;
+    public Worker(int num)
+    {
+        this.numIterations = num;
+    }
+
+    @Override
+    public Long call()
+    {
+        long circleCount = 0;
+        Random prng = new Random ();
+        for (int j = 0; j < numIterations; j++)
+        {
+            double x = prng.nextDouble();
+            double y = prng.nextDouble();
+            if ((x * x + y * y) < 1)  ++circleCount;
+        }
+        return circleCount;
+    }
+}
+
+class Master {
+    public long doRun(int totalCount, int numWorkers,int numBoucle) throws InterruptedException, ExecutionException
+    {
+        ExecutorService exec = null;
+        long total = 0;
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter("C:/Users/willi/IdeaProjects/All/Pro_Dev/src/Mesure/output_G26_4c.txt"));
+
+            for(int j=1;j<=numWorkers;j++) {
+                long startTime=0;
+                long stopTime=0;
+                double pi=0;
+                long sumTime = 0;
+                for(int k=0;k<numBoucle;k++) {
+                    startTime = System.currentTimeMillis();
+
+                    List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
+                    for (int i = 0; i < j; ++i) {
+                        tasks.add(new Worker(totalCount/j));
+                    }
+
+                    exec = Executors.newFixedThreadPool(j);
+                    List<Future<Long>> results = exec.invokeAll(tasks);
+                    total = 0;
+
+                    for (Future<Long> f : results) {
+                        total += f.get();
+                    }
+                    pi = pi+ (Math.abs(((4.0 * total / totalCount / j)- Math.PI)) / Math.PI);
+
+                    stopTime = System.currentTimeMillis();
+                    sumTime=sumTime + (stopTime - startTime);
+                    exec.shutdown();
+                }
+
+
+                writer.println(pi/numBoucle + ";" + totalCount+ ";" + j + ";" + sumTime/numBoucle);
+
+            }
+
+            writer.close();
+
+            System.out.println("Successfully wrote text to file.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return total;
+    }
+}
